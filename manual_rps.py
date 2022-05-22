d9851ce9aac2b6fc63f66cf27abcdb08da09136a
katherineyap <katherineyap25@gmail.com>
2022-05-22 19:26:58 +0100

    milestone 3 task 2

--- manual_rps.py
+++ manual_rps.py
@@ -3,16 +3,30 @@ import random
 
 option = ['rock','paper','scissors']
 
-def get_computer_choice():
+def get_winner():
     computer_choice = random.choice(option)
-    return com_choice
-
-def get_user_choice():
     user_choice = input('please select your option:')
-    return user_choice
-
-
-def get_winner(computer_choice,user_choice):
-    if computer_choice = user_choice:
+    if computer_choice == user_choice:
         print ("It's a tie")
-    elif
+
+    elif computer_choice == 'rock':
+        if user_choice == 'paper':
+            print(f'user choice is : ', user_choice,' ' + 'computer choice is : ', computer_choice,'. User wins')
+        else :
+            print(f'user choice is : ', user_choice,' ' + 'computer choice is : ', computer_choice,'. Computer wins')
+
+    elif computer_choice == 'paper':
+        if user_choice == 'rock':
+            print(f'user choice is : ', user_choice,' ' + 'computer choice is : ', computer_choice,'. Computer wins')
+        if user_choice == 'scissors':
+            print(f'user choice is : ', user_choice,' ' + 'computer choice is : ', computer_choice,'. User wins')
+
+    elif computer_choice == 'scissors':
+        if user_choice == 'rock':
+            print(f'User choice is : ', user_choice,' ' + 'computer choice is : ', computer_choice,'. User wins')
+        if user_choice == 'paper':
+            print(f'user choice is :', user_choice,' ' + 'computer choice is : ', computer_choice,'. Computer wins')
+
+    return get_winner
+
+get_winner()
