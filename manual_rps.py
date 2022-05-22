6308b4bc401b22f845bc4f82cb0471df4b326115
katherineyap <katherineyap25@gmail.com>
2022-05-22 17:16:00 +0100

    milestone 3 task1

--- manual_rps.py
+++ manual_rps.py
@@ -1,11 +1,18 @@
 
 import random
 
-option :['rock','paper','scissors']
+option = ['rock','paper','scissors']
 
-def get_computer_choice:
-    com_choice = random.choice(option)
+def get_computer_choice():
+    computer_choice = random.choice(option)
     return com_choice
 
-def get_user_choice:
-    print ('input')
+def get_user_choice():
+    user_choice = input('please select your option:')
+    return user_choice
+
+
+def get_winner(computer_choice,user_choice):
+    if computer_choice = user_choice:
+        print ("It's a tie")
+    elif
