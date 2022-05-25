312ce2e9f902c643a990e6d01234621597855951
katherineyap <katherineyap25@gmail.com>
2022-05-25 21:44:56 +0100

    task 1

--- camera_rps.py
+++ camera_rps.py
@@ -1,18 +1,13 @@
 import rps_game
 import manual_rps
+import numpy as np
 
 # 0 Scissors
 # 1 Rock
 # 2 Paper
 # 3 Nothing
+model_dic = {0:'Scissors', 1:'Rock',2: 'Paper', 3: 'Nothing'}
 
 def get_prediction(prediction):
-    if prediction[0]>[1]or[2]:
-        user_choice = 'Scissors'
-    elif prediction [1] > [0] or [2]:
-        user_choice = 'Rock'
-    elif prediction [2] > [0] or [1]:
-        user_choice = 'Paper'
-    else:
-        user_choice = 'Nothing'
-    return user_choice
+   indices = np.argmax(prediction[0])
+   user = model_dic[indices]
