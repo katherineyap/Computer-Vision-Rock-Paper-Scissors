02a2e42da9ca1dc37770a6f8a283ebdcc45ad073
katherineyap <katherineyap25@gmail.com>
2022-05-25 19:26:40 +0100

    started 'get_prediction' function but not getting the expected outcome

--- camera_rps.py
+++ camera_rps.py
@@ -1,3 +1,18 @@
-import RPS_game:
+import rps_game
+import manual_rps
 
-def get_prediction()
+# 0 Scissors
+# 1 Rock
+# 2 Paper
+# 3 Nothing
+
+def get_prediction(prediction):
+    if prediction[0]>[1]or[2]:
+        user_choice = 'Scissors'
+    elif prediction [1] > [0] or [2]:
+        user_choice = 'Rock'
+    elif prediction [2] > [0] or [1]:
+        user_choice = 'Paper'
+    else:
+        user_choice = 'Nothing'
+    return user_choice
